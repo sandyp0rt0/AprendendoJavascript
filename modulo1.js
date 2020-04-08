d7cfac2c620407750afa0b2129909b9e27b68978
Sandy <sandy.porto@gmail.com>
2020-04-08 13:53:56 -0300

    Modulo 1 terminado

--- modulo1.js
+++ modulo1.js
@@ -52,3 +52,74 @@ pares(32,321);
 
 //Modulo 1 - Exericio 3
 
+
+/*
+function temHabilidade(skills){
+
+    return(skills.includes("Javascript"));
+
+}
+
+var skills = ["Javascript", "ReactJS", "React Native"];
+
+console.log(temHabilidade(skills));
+*/
+
+//Modulo 1 - Exericio 4
+
+/*
+
+function experiencia(anos){
+    var exp;
+
+    if(anos>=0 && anos <=1){
+        exp = "Iniciante";
+    }else if(anos>1 && anos<=3){
+        exp = "Intermediário";
+    }else if(anos>3 && anos<=6){
+
+        exp = "Avançado";
+    }else if(anos>=7){
+        exp = "Jedi Master";
+    }else{
+        exp = "Valor incorreto!";
+    }
+    return(exp);
+}
+
+console.log(experiencia(1));
+console.log(experiencia(2));
+console.log(experiencia(4));
+console.log(experiencia(8));
+console.log(experiencia("oi"));
+
+*/
+
+//Modulo 1 - Exericio 5
+
+var usuarios = [
+    {
+    nome: "Diego",
+    habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+    nome: "Gabriel",
+    habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    }
+   ];
+
+
+
+function imprimeHabilidadesUsuarios(users){
+    var texto;
+    for(user of users){
+        texto = "O "+user.nome+" possui as habilidades: "+retornaHabilidades(user.habilidades);
+        console.log(texto);
+    }
+}
+
+function retornaHabilidades(arr){
+    return(arr.join(", "));
+}
+
+imprimeHabilidadesUsuarios(usuarios);
