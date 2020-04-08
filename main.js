e8faad46dc3f5fe92c514df97010086bab8f60b4
Sandy <sandy.porto@gmail.com>
2020-04-08 11:34:14 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,36 @@
+
+//Modulo 1 - Exercicio 1
+
+/*
+
+var endereco = {
+    rua: "rua A",
+    numero: 1293,
+    bairro: "Bairro B",
+    cidade: "Cidade C",
+    uf: "Estado D"
+};
+
+function imprimeEndereco(end){
+    var texto = "O usuário mora em ";
+    texto = texto + end.cidade + "/" + end.uf;
+    texto = texto + ", no bairro "+end.bairro;
+    texto = texto + ", na rua \""+end.rua + "\"";
+    texto = texto + " com nº"+end.numero;
+
+    return(texto);
+
+}
+
+console.log(imprimeEndereco(endereco)); 
+
+var div = document.querySelector(".app");
+
+var label = document.createElement("label");
+
+var texto = document.createTextNode(imprimeEndereco(endereco));
+
+label.appendChild(texto);
+div.appendChild(label);
+*/
+
