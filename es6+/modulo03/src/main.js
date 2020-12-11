2caff7466b4049a6b3790ef48ac9c02925619563
Sandy Porto <sandyporto@gmail.com>
2020-12-11 11:38:32 -0300

    Codigo 03 do modulo 03

--- es6+/modulo03/src/main.js
+++ es6+/modulo03/src/main.js
@@ -52,7 +52,7 @@ function getUserFromGithub(user) {
 
 getUserFromGithub("diego3g");
 getUserFromGithub("sandyp0rt0");
-getUserFromGithub("hn8ed873");
+//getUserFromGithub("hn8ed873");
 
 
 
@@ -70,7 +70,7 @@ class Api {
 
 Api.getUserInfo("diego3g");
 Api.getUserInfo("sandyp0rt0");
-Api.getUserInfo("diego3g9eu8h83");
+//Api.getUserInfo("diego3g9eu8h83");
 
 //ou ainda
 
@@ -84,4 +84,69 @@ const api = async (user) => {
 
 api("diego3g");
 api("sandyp0rt0");
-api("ijnd0983");
+//api("ijnd0983");
+
+
+console.log("Codigo 03");
+
+class Github {
+    static getRepositories(repo){
+        axios.get(`https://api.github.com/repos/${repo}`)
+            .then(response => {
+                //console.log(response.data);
+                 const git = [response.data.name,
+                    response.data.description,
+                    response.data.url];
+                console.log(git);
+            })
+            .catch(err => {
+                console.log(err);
+                console.log(`Repositório ${repo} não existe.`);
+            })
+    }
+}
+
+//Github.getRepositories("rocketseat/rocketseat.com.br");
+Github.getRepositories("sandyp0rt0/AprendendoJavascript");
+//Github.getRepositories("rocketseat/dslkmskv");
+
+
+class Github2 {
+    static async getRepositories(repo){
+        try{
+            const response = await axios.get(`https://api.github.com/repos/${repo}`);
+            const git = [response.data.name,
+                        response.data.description,
+                        response.data.url];
+            console.log(git);
+        }catch(err){
+            console.log(err);
+            console.log(`Repositório ${repo} não existe.`);
+        }
+        
+
+    }
+}
+
+
+Github2.getRepositories("sandyp0rt0/AprendendoJavascript");
+
+//ou ainda
+
+const getRepositorio = async (repo) => {
+    try{
+        const response = await axios.get(`https://api.github.com/repos/${repo}`);
+        const git = [response.data.name,
+                    response.data.description,
+                    response.data.url];
+        console.log(git);
+    }catch (err){
+        console.log(err);
+        console.log(`Reposítório ${repo} não existe`);
+    }
+}
+
+getRepositorio("sandyp0rt0/AprendendoJavascript");
+
+
+console.log("Codigo 04");
